226d4da68fcb4ece2d89ac52b285c2bc86073342
PolinaSavelyeva <lissava00@gmail.com>
2022-10-02 05:29:01 +0300

    + OOP types

--- src/ProgrammingHomeworkFs/Main.fs
+++ src/ProgrammingHomeworkFs/Main.fs
@@ -1,14 +1,15 @@
 module ProgrammingHomeworkFs
 
-(*Объявление нового типа t5 (списка), являющегося декартовым произведением числа * строки * булевого значения * числа
+//Объявление нового типа t5 (списка), являющегося декартовым произведением числа * строки * булевого значения * числа
 type t5 = int * string * bool * int
+
 // Создание переменной f типа t5
 let f: t5 = (1, "3", true, 3)
+
 // Создание функции g, принимающей значение типа t5 и возвращающей список : (2,"1",false,5)
 let g (x: t5) =
-    // Разбиение на соответствия с помощью match with
-    match x with
-    |_, _, _, _ -> (2, "1", false, 5)*)
+    match x with  // Разбиение на соответствия с помощью match with
+    |_, _, _, _ -> (2, "1", false, 5)
 
 //------------------------------------------------------АЛГЕБРАИЧЕСКИЙ-ТИП--------------------------
 // Алгебраический тип MyList- перемножаем декартово
@@ -70,7 +71,7 @@ let rec oopMap (f: IActor<'value, 'result>) (lst: IList<'value>) =
         let lst1 = lst :?> MyOOPNonEmptyList<'value> // :?> оператор преобразует тип в тип, находящийся на более низком уровне иерархии
         MyOOPNonEmptyList(f.Do lst1.Head, oopMap f lst1.Tail)
     else
-        failwith "!!!"
+        failwith "Undefined in : oopMap"
 
 // Рекурсивная функция oopMap2, аналогичная функции oopMap1
 let rec oopMap2 (f: IActor<'value, 'result>) (lst: IList<'value>) =
@@ -78,6 +79,7 @@ let rec oopMap2 (f: IActor<'value, 'result>) (lst: IList<'value>) =
     | :? MyOOPEmptyList<'value> -> MyOOPEmptyList() :> IList<'result> // преобразование необходимо для устранения конфликта между разными типами, получаемыми из функции
     | :? MyOOPNonEmptyList<'value> as lst -> // разные lst
         MyOOPNonEmptyList(f.Do lst.Head, oopMap f lst.Tail)
+    | _ -> failwith "Undefined in : oopMap2"
 
 // Функция go2, вызывающая функцию oopMap, которая к каждому элементу прибавляет +1
 let go2 () =
@@ -99,12 +101,7 @@ let rec fromMyListToMyOOPList lst =
     | Empty -> MyOOPEmptyList<'value>() :> IList<'value>
     | Construct (hd, tl) -> MyOOPNonEmptyList<'value>(hd, fromMyListToMyOOPList tl)
 
-//printf $"%A{g (f)}"
 //------------------------------------------------------№1----АЛГЕБРАИЧЕСКИЙ-ТИП--------------------
-
-let primer: MyList<int> =
-    Construct(48, Construct(0, Construct(0, Construct(734, Construct(-880, Construct(9, Empty))))))
-
 let rec lenMyList (lst: MyList<'value>) : int =
     match lst with
     | Empty -> 0
@@ -129,23 +126,13 @@ let bubbleSort (lst: MyList<'value>) : MyList<'value> =
 
     sortedLine
 
-//printf $"%A{bubbleSort primer}"
 //------------------------------------------------------№3----АЛГЕБРАИЧЕСКИЙ-ТИП--------------------
 let rec concat (lst1: MyList<'value>) (lst2: MyList<'value>) : MyList<'value> =
     match lst1 with
     | Empty -> lst2
     | Construct (hd, tl) -> Construct(hd, concat tl lst2)
+
 //------------------------------------------------------№2----АЛГЕБРАИЧЕСКИЙ-ТИП--------------------
-// Функция возвращает hd от типа MyList
-(*let takeHead (lst : MyList<'value>) : 'value =
-    match lst with
-    | Construct (hd, _) -> hd
-    | _ -> failwith "Undefined"*)
-
-let transLst (x: 'value) : MyList<'value> =
-    match x with
-    | x -> Construct(x, Empty)
-
 let rec leftLst (x: 'value) (lst: MyList<'value>) : MyList<'value> =
     match lst with
     | Empty -> Empty
@@ -174,48 +161,32 @@ let rec rightLst (x: 'value) (lst: MyList<'value>) : MyList<'value> =
         else
             rightLst x tl
 
-(*let rec middleLst (x : 'value) (lst : MyList<'value>) : MyList<'value> =
-    match lst with
-    | Empty -> Empty
-    | Construct (hd, Empty) ->
-        if hd = x then Construct (hd, Empty)
-        else Empty
-    | Construct (hd1, Construct(hd2, tl)) ->
-        if hd1 = x then Construct(hd1, rightLst x tl)
-        else Construct(hd2, tl)*)
-
 let rec quickSort (lst: MyList<'value>) : MyList<'value> =
     match lst with
     | Empty -> Empty
     | Construct (hd, Empty) -> Construct(hd, Empty)
-    | Construct (hd, tl) -> concat (concat (quickSort (leftLst hd tl)) (transLst hd)) (quickSort (rightLst hd tl))
-
-//printf $"%A{quickSort primer}"
+    | Construct (hd, tl) -> concat (concat (quickSort (leftLst hd tl)) (Construct(hd, Empty))) (quickSort (rightLst hd tl))
 
 //------------------------------------------------------№1----ООП-ТИП-------------------------------
-
-let oopPrimer =
-    MyOOPNonEmptyList(1, MyOOPNonEmptyList(3, MyOOPNonEmptyList(-9, MyOOPEmptyList())))
-
 let rec lenMyOOPList (lst: IList<'value>) : int =
     match lst with
     | :? MyOOPEmptyList<'value> -> 0
     | :? MyOOPNonEmptyList<'value> as lst -> lenMyOOPList lst.Tail + 1
-
-//printf $"%A{lenMyOOPList oopPrimer}"
+    | _ -> failwith "Undefined in : lenMyOOPList"
 
 let takeOOPHead (lst: IList<'value>) : 'value =
     match lst with
     | :? MyOOPNonEmptyList<'value> as lst -> lst.Head
+    | _ -> failwith "Undefined in : takeOOPHead"
 
 let takeOOPTail (lst: IList<'value>) : IList<'value> =
     match lst with
     | :? MyOOPNonEmptyList<'value> as lst ->
         if lst.Tail :? MyOOPEmptyList<'value> then
-            lst
+            MyOOPEmptyList()
         else
             lst.Tail
-    | _ -> failwith "!!!"
+    | _ -> failwith "Undefined in : takeOOPTail"
 
 let rec oneOOPLine (lst: IList<'value>) : IList<'value> =
     match lst with
@@ -227,25 +198,70 @@ let rec oneOOPLine (lst: IList<'value>) : IList<'value> =
             MyOOPNonEmptyList(takeOOPHead lst.Tail, oneOOPLine (MyOOPNonEmptyList(lst.Head, takeOOPTail lst.Tail)))
         else
             MyOOPNonEmptyList(lst.Head, oneOOPLine lst.Tail)
-    | _ -> failwith "!!!"
+    | _ -> failwith "Undefined in : oneOOPLine"
 
+let bubbleOOPSort (lst: IList<'value>) : IList<'value> =
+    let mutable sortedLine: IList<'value> = lst
 
-printf $"%A{oneOOPLine oopPrimer}"
-
-(*let bubbleSort (lst: MyList<'value>) : MyList<'value> =
-    let mutable sortedLine: MyList<'value> = lst
-
-    for i in 0 .. (lenMyList lst - 2) do
-        sortedLine <- oneLine sortedLine
+    for i in 0 .. (lenMyOOPList lst - 2) do
+        sortedLine <- oneOOPLine sortedLine
 
     sortedLine
-*)
-//------------------------------------------------------№3----ООП-ТИП-------------------------------
 
+//------------------------------------------------------№3----ООП-ТИП-------------------------------
 let rec concatOOP (lst1: IList<'value>) (lst2: IList<'value>) : IList<'value> =
     match lst1 with
     | :? MyOOPEmptyList<'value> -> lst2
     | :? MyOOPNonEmptyList<'value> as lst1 -> MyOOPNonEmptyList(lst1.Head, concatOOP lst1.Tail lst2) :> IList<'value>
-    | _ -> failwith "!!!"
+    | _ -> failwith "Undefined in : concatOOP"
 
-//printf $"%A{concatOOP oopPrimer oopPrimer}"
+ //------------------------------------------------------№2----АЛГЕБРАИЧЕСКИЙ-ТИП--------------------
+let rec leftOOPLst (x: 'value) (lst: IList<'value>) : IList<'value> =
+    match lst with
+
+    | :? MyOOPEmptyList<'value> -> MyOOPEmptyList() :> IList<'value>
+    | :? MyOOPNonEmptyList<'value> as lst ->
+        if lst.Tail :? MyOOPEmptyList<'value> then
+            if lst.Head <= x then lst
+            else MyOOPEmptyList() :> IList<'value>
+        else
+            if lst.Head <= x then MyOOPNonEmptyList(lst.Head, leftOOPLst x (MyOOPNonEmptyList(x, lst.Tail)))
+            else leftOOPLst x lst.Tail
+    | _ -> failwith "Undefined in : leftOOPLst"
+
+let rec rightOOPLst (x: 'value) (lst: IList<'value>) : IList<'value> =
+    match lst with
+
+    | :? MyOOPEmptyList<'value> -> MyOOPEmptyList() :> IList<'value>
+    | :? MyOOPNonEmptyList<'value> as lst ->
+        if lst.Tail :? MyOOPEmptyList<'value> then
+            if lst.Head > x then lst
+            else MyOOPEmptyList() :> IList<'value>
+        else
+            if lst.Head > x then MyOOPNonEmptyList(lst.Head, rightOOPLst x (MyOOPNonEmptyList(x, lst.Tail)))
+            else rightOOPLst x lst.Tail
+    | _ -> failwith "Undefined in : rightOOPLst"
+
+let rec quickOOPSort (lst:IList<'value>) : IList<'value> =
+    match lst with
+    | :? MyOOPEmptyList<'value> -> MyOOPEmptyList() :> IList<'value>
+    | :? MyOOPNonEmptyList<'value> as lst ->
+        if lst.Tail :? MyOOPEmptyList<'value> then lst
+        else concatOOP (concatOOP (quickOOPSort (leftOOPLst lst.Head lst.Tail)) (MyOOPNonEmptyList(lst.Head, MyOOPEmptyList()))) (quickOOPSort (rightOOPLst lst.Head lst.Tail))
+    | _ -> failwith "Undefined in : quickOOPSort"
+
+let rec fromMyOOPListToMyList (lst : IList<'value>) : MyList<'value> =
+    match lst with
+    | :? MyOOPEmptyList<'value> -> Empty
+    | :? MyOOPNonEmptyList<'value> as lst->
+        let hd = lst.Head
+        let tl = lst.Tail
+        Construct(hd, fromMyOOPListToMyList tl)
+    | _ -> failwith "Undefined in : fromMyOOPListToMyList"
+
+
+(*let primer: MyList<int> =
+    Construct(48, Construct(0, Construct(0, Construct(734, Construct(-880, Construct(9, Empty))))))
+  let oopPrimer =
+    MyOOPNonEmptyList(1, MyOOPNonEmptyList(3, MyOOPNonEmptyList(-9, MyOOPNonEmptyList(-100, MyOOPNonEmptyList(87, MyOOPNonEmptyList(-300, MyOOPNonEmptyList(0, MyOOPNonEmptyList(0, MyOOPEmptyList()))))))))
+*)
