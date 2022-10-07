e75c10f55dd68bb2fb6b2d15adad94f0fc28b04f
PolinaSavelyeva <lissava00@gmail.com>
2022-10-07 03:04:33 +0300

    Update MegaTree.fs

--- src/ProgrammingHomeworkFs/MegaTree.fs
+++ src/ProgrammingHomeworkFs/MegaTree.fs
@@ -4,33 +4,26 @@ open AbstractTrees
 // в котором внутренний узел может иметь произвольное количество сыновей.
 // Листья и внутренние узлы хранят некоторые значени произвольного (одинакового для всех) типа.
 
-// Функция создает тип параметра
-
-(*type BinTree<'value> =
-    | Node of value : 'value * minNode : myOption<BinTree<'value>>
-
-and myOption<'a> =
-    | FullNodeExist of myOption<'a>
-    | NodeExist of 'a // Ветка со значением и без потомка
-    | None
-
-let tree = Node (1, None)
-let tree2 = Node(1, NodeExist(Node(1, None)))*)
-type MyNodeList<'value> =
-    | Cons of head: 'value * tail: MyNodeList<'value>
-    | Empty
-
-type MyTree<'value> =
-    | Branch of 'value * MyNodeList<'value>
+type Tree<'value> =
+    | Node of ('value * Tree<'value> list)
     | Leaf of 'value
 
-(*let howManyNodes (a : 'value) =
-     if a > 0 then
-         for
-         Some (BinTree<'value> * predNode)
-     else
-         None*)
-// Декартово произведение значения * есть/нет ветка 1 * есть/нет ветка 2 * есть/нет ветка 3 * ....
-// число сыновей или None
-// если число сыновей то создается столько бинтри, сколько сыновей указано
-// Если None, то
+let probTree = Node(12, [Node(2, [Leaf(9); Leaf(21)]); Leaf(5)])
+//        12
+//      2   5
+//     9 21
+
+//2. Реализовать функцию, которая по дереву (тип, описанный выше),
+//находит количество различных элементов, хранящихся в узлах. Использовать рекурсию,
+//не использовать мутабельные переменные.
+
+let rec countElTree (tree : Tree<'value>) : int =
+    match tree with
+    | Leaf _-> 1
+    | Node (_, tl) -> 1 +  countElList tl
+
+let rec countElList (lst : list<'value>) : int =
+    match lst with
+    | [x] -> 1
+    | [_;x] ->
+        1 + countElTree x
