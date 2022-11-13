d9532b370179f11545e058774e19f62149b4fe4e
PolinaSavelyeva <lissava00@gmail.com>
2022-11-13 12:02:25 +0300

    - fantomasignore - comments

--- src/ProgrammingHomeworkFs/SimpleFunctions.fs
+++ src/ProgrammingHomeworkFs/SimpleFunctions.fs
@@ -1,24 +1,17 @@
 module ProgrammingHomeworkFs
 
-/// Функция  expo возводит указанное число в указанную степень
 let expo (basement: float) (exponent: int) : float =
     if basement = 0.0 && exponent = 0 then
-        failwith
-            "Don't try to raise zero to the power of zero. Expected both non-zero basement and non-zero exponent. \n Error when calling - expo - function."
+        failwith "Don't try to raise zero to the power of zero. Expected both non-zero basement and non-zero exponent. \n Error when calling - expo - function."
     elif basement = 0.0 && exponent < 0 then
-        failwith
-            "Don't try to raise zero to the negative power. Expected zero basement and positive <> 0 exponent. \n Error when calling - expo - function."
+        failwith "Don't try to raise zero to the negative power. Expected zero basement and positive <> 0 exponent. \n Error when calling - expo - function."
     elif basement = 0.0 then
         0.0
     else
         let mutable ans = 1.0
-        let mutable counter = abs exponent // счетчик для цикла
+        let mutable counter = abs exponent
 
-        let product = // product будет многократно умножаться на себя
-            if exponent > 0 then
-                basement
-            else
-                1.0 / basement
+        let product = if exponent > 0 then basement else 1.0 / basement
 
         while counter > 0 do
             ans <- ans * product
@@ -26,27 +19,23 @@ let expo (basement: float) (exponent: int) : float =
 
         ans
 
-/// Функция fastExpo использует быстрое возведение в степень
-let rec fastExpo (basement: float) (exponent: int) : float = // exponent >= 0
+let rec fastExpo (basement: float) (exponent: int) : float =
     if basement = 0.0 && exponent = 0 then
-        failwith
-            "Don't try to raise zero to the power of zero. Expected both non-zero basement and non-zero exponent. \n Error when calling - fastExpo - function"
+        failwith "Don't try to raise zero to the power of zero. Expected both non-zero basement and non-zero exponent. \n Error when calling - fastExpo - function"
     elif basement = 0.0 && exponent < 0 then
-        failwith
-            "Don't try to raise zero to the negative power. Expected zero basement and positive <> 0 exponent. \n Error when calling - fastExpo - function."
+        failwith "Don't try to raise zero to the negative power. Expected zero basement and positive <> 0 exponent. \n Error when calling - fastExpo - function."
     elif exponent = 1 then
         basement
     elif exponent = 0 then
         1.0
     else
-        let body = fastExpo basement (exponent / 2) // сохраняем предыдущее значение рекурсивной функции
+        let body = fastExpo basement (exponent / 2)
 
         if exponent % 2 = 0 then
             body * body
         else
             body * body * basement
 
-/// Функция inline возвращает разницу между максимальным и минимальным элементами массива
 let inline arrays (arr: 'value array) =
     if arr.Length > 0 then
         let mutable maximum = arr[0]
@@ -60,16 +49,15 @@ let inline arrays (arr: 'value array) =
 
         maximum - minimum
     else
-        failwith
-            "There is no max and min elements in empty array. Expected at least 1 element in array. \n Error when calling - arrays - function"
+        failwith "There is no max and min elements in empty array. Expected at least 1 element in array. \n Error when calling - arrays - function"
 
-/// Функция odds возвращает массив целый нечетных чисел, лежащих строго между введенными числами
 let odds (x: int) (y: int) =
     let minim = if x < y then x else y
     let maxim = if x > y then x else y
 
     let odds_array =
-        [| for i in minim + minim % 2 + 1 .. 2 .. maxim - maxim % 2 do yield i |]
+        [| for i in minim + minim % 2 + 1 .. 2 .. maxim - maxim % 2 do
+               yield i |]
 
     odds_array
 
