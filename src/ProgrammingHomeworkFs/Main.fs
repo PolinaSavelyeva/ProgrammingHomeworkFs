c7fda64c1c0aefb498dfeef624ec823db5da127b
PolinaSavelyeva <lissava00@gmail.com>
2022-11-17 22:23:26 +0300

    upd

--- src/ProgrammingHomeworkFs/Main.fs
+++ src/ProgrammingHomeworkFs/Main.fs
@@ -1,6 +1,19 @@
 namespace ProgrammingHomeworkFs
 
+open SparseMatrix
+open SparseVector
+open MatrixMultiplication
 module Main =
 
     [<EntryPoint>]
-    let main (argv: string array) = 0
+    let main (argv: string array) =
+        let arrSome = Vector([|Option.None; Some 1; Option.None; Some 2|])
+
+        let tableSome = Matrix(array2D
+                            [[Some 7; Option.None; Option.None; Option.None; Option.None]
+                             [Some 7; Option.None; Option.None; Option.None; Some 4]
+                             [Option.None; Option.None; Option.None; Option.None; Option.None]
+                             [Option.None; Some 8; Some 1; Some 9; Option.None]])
+        let res = multiplication (+) (*) arrSome tableSome
+        printf $"%A{res.Storage}"
+        0
