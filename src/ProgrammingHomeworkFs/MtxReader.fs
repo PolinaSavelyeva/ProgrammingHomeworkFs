aec69d21dcfa4a1a2f311e9d6450ac02a842b841
PolinaSavelyeva <lissava00@gmail.com>
2023-02-09 20:56:16 +0500

    Fix formatting

--- src/ProgrammingHomeworkFs/MtxReader.fs
+++ src/ProgrammingHomeworkFs/MtxReader.fs
@@ -2,7 +2,7 @@ module MtxReader
 
 open SparseMatrix
 
-type MtxFile (pathToFile : string) =
+type MtxFile(pathToFile: string) =
 
     let rec commentsCutter (array: array<string>) =
         if array.Length = 0 then
@@ -16,13 +16,13 @@ type MtxFile (pathToFile : string) =
 
     let array = System.IO.File.ReadAllLines pathToFile
     let zeroArrayLine = array[ 0 ].Split()
-    let weight = zeroArrayLine[3].ToLower()
-    let symmetry = zeroArrayLine[4].ToLower() = "symmetric"
+    let weight = zeroArrayLine[ 3 ].ToLower()
+    let symmetry = zeroArrayLine[ 4 ].ToLower() = "symmetric"
     let cutArray = commentsCutter array
     let zeroCutArrayLine = cutArray[ 0 ].Split()
     let rows = uint zeroCutArrayLine[0]
     let columns = uint zeroCutArrayLine[1]
-    
+
     member this.Array = array
     member this.Weight = weight
     member this.IsMtxSymmetric = symmetry
@@ -32,39 +32,38 @@ type MtxFile (pathToFile : string) =
 
 let symmetryIntMtxToGraph (array: array<string>) rows columns =
 
-        let rec listOfIntFormation (array: array<string>) =
-            if array.Length = 0 then
-                []
-            else
-                let currentLine = array[ 0 ].Split()
+    let rec listOfIntFormation (array: array<string>) =
+        if array.Length = 0 then
+            []
+        else
+            let currentLine = array[ 0 ].Split()
 
-                (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Int32.Parse(currentLine[2])))
-                :: (listOfIntFormation array[1..])
+            (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Int32.Parse(currentLine[2])))
+            :: (listOfIntFormation array[1..])
 
-        Matrix(listOfIntFormation array, rows, columns)
+    Matrix(listOfIntFormation array, rows, columns)
 
 let symmetryDoubleMtxToGraph (array: array<string>) rows columns =
 
-        let rec listOfDoubleFormation (array: array<string>) =
-            if array.Length = 0 then
-                []
-            else
-                let currentLine = array[ 0 ].Split()
+    let rec listOfDoubleFormation (array: array<string>) =
+        if array.Length = 0 then
+            []
+        else
+            let currentLine = array[ 0 ].Split()
 
-                (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Double.Parse(currentLine[2])))
-                :: (listOfDoubleFormation array[1..])
+            (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Double.Parse(currentLine[2])))
+            :: (listOfDoubleFormation array[1..])
 
-        Matrix(listOfDoubleFormation array, rows, columns)
+    Matrix(listOfDoubleFormation array, rows, columns)
 
 let symmetryPatternMtxToGraph (array: array<string>) rows columns =
 
-        let rec listOfUnitFormation (array: array<string>) =
-            if array.Length = 0 then
-                []
-            else
-                let currentLine = array[ 0 ].Split()
+    let rec listOfUnitFormation (array: array<string>) =
+        if array.Length = 0 then
+            []
+        else
+            let currentLine = array[ 0 ].Split()
 
-                (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some())
-                :: (listOfUnitFormation array[1..])
+            (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some()) :: (listOfUnitFormation array[1..])
 
-        Matrix(listOfUnitFormation array, rows, columns)
+    Matrix(listOfUnitFormation array, rows, columns)
