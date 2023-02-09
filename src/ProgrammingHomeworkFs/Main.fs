d7c93893dbc6223e0a68c8e89e546a301d8eaceb
PolinaSavelyeva <lissava00@gmail.com>
2023-02-09 18:35:44 +0500

    Fantomas

--- src/ProgrammingHomeworkFs/Main.fs
+++ src/ProgrammingHomeworkFs/Main.fs
@@ -5,9 +5,4 @@ open MtxReader
 module Main =
 
     [<EntryPoint>]
-    let main _ =
-        let array =
-            readLines "/Users/polinas/Documents/ProgrammingHomeworkFs/src/ProgrammingHomeworkFs/Matrices/494_bus.mtx"
-
-        printf $"%A{(mtxToGraphMatrix array).Storage}"
-        0
+    let main _ = 0
