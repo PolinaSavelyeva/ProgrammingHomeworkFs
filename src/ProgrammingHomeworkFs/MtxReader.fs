2beab7accb5756d4a368008c367e53a87cdf3617
PolinaSavelyeva <lissava00@gmail.com>
2023-02-09 20:36:31 +0500

    Add new MtxFile type

--- src/ProgrammingHomeworkFs/MtxReader.fs
+++ src/ProgrammingHomeworkFs/MtxReader.fs
@@ -2,98 +2,69 @@ module MtxReader
 
 open SparseMatrix
 
-let readLines pathToFile =
-    let lines = System.IO.File.ReadAllLines pathToFile
-    lines
-
-let symmetryIntMtxToGraph (array: array<string>) =
+type MtxFile (pathToFile : string) =
 
     let rec commentsCutter (array: array<string>) =
         if array.Length = 0 then
             failwith
                 "Incorrect file was given. Expected MatrixMarket format file.\n
-                      Error in -symmetryIntMtxToGraph- function"
+                 Error in -commentsCutter- function"
         else if array[0][0] <> '%' then
             array
         else
             commentsCutter array[1..]
 
-    let matrixFormation (array: array<string>) =
+    let array = System.IO.File.ReadAllLines pathToFile
+    let zeroArrayLine = array[ 0 ].Split()
+    let weight = zeroArrayLine[3].ToLower()
+    let symmetry = zeroArrayLine[4].ToLower() = "symmetric"
+    let cutArray = commentsCutter array
+    let zeroCutArrayLine = cutArray[ 0 ].Split()
+    let rows = uint zeroCutArrayLine[0]
+    let columns = uint zeroCutArrayLine[1]
+    
+    member this.Array = array
+    member this.Weight = weight
+    member this.IsMtxSymmetric = symmetry
+    member this.CutArray = cutArray
+    member this.Rows = rows
+    member this.Columns = columns
 
-        let firstLine = array[ 0 ].Split()
-        let rows = uint firstLine[0]
-        let columns = uint firstLine[1]
+let symmetryIntMtxToGraph (array: array<string>) rows columns =
 
-        let rec listFormation (array: array<string>) =
+        let rec listOfIntFormation (array: array<string>) =
             if array.Length = 0 then
                 []
             else
                 let currentLine = array[ 0 ].Split()
 
                 (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Int32.Parse(currentLine[2])))
-                :: (listFormation array[1..])
+                :: (listOfIntFormation array[1..])
 
-        Matrix(listFormation array[1..], rows, columns)
+        Matrix(listOfIntFormation array, rows, columns)
 
-    commentsCutter array |> matrixFormation
+let symmetryDoubleMtxToGraph (array: array<string>) rows columns =
 
-let symmetryDoubleMtxToGraph (array: array<string>) =
-
-    let rec commentsCutter (array: array<string>) =
-        if array.Length = 0 then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                      Error in -symmetryDoubleMtxToGraph- function"
-        else if array[0][0] <> '%' then
-            array
-        else
-            commentsCutter array[1..]
-
-    let matrixFormation (array: array<string>) =
-
-        let firstLine = array[ 0 ].Split()
-        let rows = uint firstLine[0]
-        let columns = uint firstLine[1]
-
-        let rec listFormation (array: array<string>) =
+        let rec listOfDoubleFormation (array: array<string>) =
             if array.Length = 0 then
                 []
             else
                 let currentLine = array[ 0 ].Split()
 
                 (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some(System.Double.Parse(currentLine[2])))
-                :: (listFormation array[1..])
+                :: (listOfDoubleFormation array[1..])
 
-        Matrix(listFormation array[1..], rows, columns)
+        Matrix(listOfDoubleFormation array, rows, columns)
 
-    commentsCutter array |> matrixFormation
+let symmetryPatternMtxToGraph (array: array<string>) rows columns =
 
-let symmetryPatternMtxToGraph (array: array<string>) =
-
-    let rec commentsCutter (array: array<string>) =
-        if array.Length = 0 then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                      Error in -symmetryPatternMtxToGraph- function"
-        else if array[0][0] <> '%' then
-            array
-        else
-            commentsCutter array[1..]
-
-    let matrixFormation (array: array<string>) =
-
-        let firstLine = array[ 0 ].Split()
-        let rows = uint firstLine[0]
-        let columns = uint firstLine[1]
-
-        let rec listFormation (array: array<string>) =
+        let rec listOfUnitFormation (array: array<string>) =
             if array.Length = 0 then
                 []
             else
                 let currentLine = array[ 0 ].Split()
 
-                (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some()) :: (listFormation array[1..])
+                (uint currentLine[0] - 1u, uint currentLine[1] - 1u, Some())
+                :: (listOfUnitFormation array[1..])
 
-        Matrix(listFormation array[1..], rows, columns)
-
-    commentsCutter array |> matrixFormation
+        Matrix(listOfUnitFormation array, rows, columns)
