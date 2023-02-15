2fe602c4527378e1f31a7ffbcc19602ea4509716
PolinaSavelyeva <lissava00@gmail.com>
2023-02-15 22:19:25 +0300

    Fix benchmark parameters, add new benchmark report

--- src/ProgrammingHomeworkFs/BenchmarkExperiments.fs
+++ src/ProgrammingHomeworkFs/BenchmarkExperiments.fs
@@ -4,14 +4,14 @@ open BenchmarkDotNet.Attributes
 
 let random = System.Random()
 
-let plusOperation opt1 opt2 =
+let IntPlusOperation opt1 opt2 =
     match opt1, opt2 with
     | Option.Some a, Option.Some b -> Option.Some(a + b)
     | Option.Some a, Option.None
     | Option.None, Option.Some a -> Option.Some(a)
     | Option.None, Option.None -> Option.None
 
-let multiOperation opt1 opt2 =
+let IntMultiOperation opt1 opt2 =
     match opt1, opt2 with
     | Some a, Some b -> Some(a * b)
     | Option.None, _
@@ -22,26 +22,31 @@ type AdditionBenchmark() =
     let mutable vector1 = SparseVector.Vector([||])
     let mutable vector2 = SparseVector.Vector([||])
 
-    [<Params(2_500_000u)>]
+    [<Params(1_000_000u, 2_500_000u)>]
     member val Length = 0u with get, set
 
-    [<Params(2u, 4u)>]
-    member val Level = 0u with get, set
+    [<Params(0u, 1u, 2u, 3u ,4u)>]
+    member val ParallelLevel = 0u with get, set
+
+    // Greater level equals more matrix's sparsity
+    // Level 1 equals dense matrix
+    [<Params(1, 2, 3)>]
+    member val DensityLevel = 1 with get, set
 
     [<GlobalSetup>]
-    member this.SetUpVectorAndMatrix() =
-        vector1 <- SparseVector.Vector(Array.init (Converters.toInt this.Length) (fun _ -> Some(random.Next())))
-        vector2 <- SparseVector.Vector(Array.init (Converters.toInt this.Length) (fun _ -> Some(random.Next())))
+    member this.SetUpVectors() =
+            vector1 <- SparseVector.Vector(Array.init (Converters.toInt this.Length) (fun n -> if n % this.DensityLevel <> 0 then Option.None else Some(random.Next())))
+            vector2 <- SparseVector.Vector(Array.init (Converters.toInt this.Length) (fun n -> if n % this.DensityLevel <> 0 then Option.None else Some(random.Next())))
 
     [<Benchmark(Baseline = true)>]
     member this.BaselineAddition() =
-        SparseVector.vectorAddition <| plusOperation <| vector1 <| vector2
+        SparseVector.vectorAddition <| IntPlusOperation <| vector1 <| vector2
 
     [<Benchmark>]
     member this.ParallelAddition() =
         ParallelMatrixAndVectorOperations.vectorAddition
-        <| this.Level
-        <| plusOperation
+        <| this.ParallelLevel
+        <| IntPlusOperation
         <| vector1
         <| vector1
 
@@ -50,26 +55,32 @@ type MultiplicationBenchmark() =
     let mutable vector = SparseVector.Vector([||])
     let mutable matrix = SparseMatrix.Matrix(array2D [||])
 
-    [<Params(3000u)>]
-    member val Length = 0u with get, set
+    [<Params(1000u, 3000u)>]
+    member val Length1 = 0u with get, set
 
-    [<Params(2u, 4u)>]
-    member val Level = 0u with get, set
+    [<Params(500u, 1000u, 2000u)>]
+    member val Length2 = 0u with get, set
+
+    [<Params(0u, 1u, 2u, 3u ,4u)>]
+    member val ParallelLevel = 0u with get, set
+
+    [<Params(1, 2, 3)>]
+    member val DensityLevel = 1 with get, set
 
     [<GlobalSetup>]
     member this.SetUpVectorAndMatrix() =
-        vector <- SparseVector.Vector(Array.init (Converters.toInt this.Length) (fun _ -> Some(random.Next())))
-        matrix <- SparseMatrix.Matrix(Array2D.init (Converters.toInt this.Length) (Converters.toInt this.Length) (fun _ _ -> Some(random.Next())))
+        vector <- SparseVector.Vector(Array.init (Converters.toInt this.Length1) (fun n -> if n % this.DensityLevel <> 0 then Option.None else Some(random.Next())))
+        matrix <- SparseMatrix.Matrix(Array2D.init (Converters.toInt this.Length1) (Converters.toInt this.Length2) (fun n _ -> if n % this.DensityLevel <> 0 then Option.None else Some(random.Next())))
 
     [<Benchmark(Baseline = true)>]
     member this.BaselineMultiplication() =
-        MatrixMultiplication.multiplication <| plusOperation <| multiOperation <| vector <| matrix
+        MatrixMultiplication.multiplication <| IntPlusOperation <| IntMultiOperation <| vector <| matrix
 
     [<Benchmark>]
     member this.ParallelMultiplication() =
         ParallelMatrixAndVectorOperations.multiplication
-        <| this.Level
-        <| plusOperation
-        <| multiOperation
+        <| this.ParallelLevel
+        <| IntPlusOperation
+        <| IntMultiOperation
         <| vector
         <| matrix
--- src/ProgrammingHomeworkFs/MtxReader.fs
+++ src/ProgrammingHomeworkFs/MtxReader.fs
@@ -2,111 +2,38 @@ module MtxReader
 
 open SparseMatrix
 
-type MtxFile(pathToFile: string) =
+(*type MtxFile(pathToFile: string) =
 
-    let rec commentsCutter (array: array<string>) =
-        if array.Length = 0 then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                 Error in -commentsCutter- function"
-        else if array[0][0] <> '%' then
-            array
-        else
-            commentsCutter array[1..]
+    let allLinesSeq = seq { yield! System.IO.File.ReadLines pathToFile }
+    let dataLinesSeq = Seq.skipWhile (fun (n : string) -> n[0] = '%') allLinesSeq
+    let firstSplitLine = (Seq.head allLinesSeq).Split()
+    let firstSplitDataLine = (Seq.head dataLinesSeq).Split()
+    let weightType = firstSplitLine[ 3 ].ToLower()
+    let isSymmetric = firstSplitLine[ 4 ].ToLower() = "symmetric"
+    let rows = uint firstSplitDataLine[0]
+    let columns = uint firstSplitDataLine[1]
 
-    let array = System.IO.File.ReadAllLines pathToFile
+    let fromMtxToSparseMatrix converter =
+        let rec tripleListFormation (sequence: seq<string>) =
+                if Seq.length sequence = 0 then
+                    []
+                else
+                    let currentLine = (Seq.head sequence).Split()
+                    let vertexOne = uint currentLine[0] - 1u
+                    let vertexTwo = uint currentLine[1] - 1u
 
-    let zeroArrayLine =
-        if Array.tryHead array = Option.None then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                 Error in -zeroArrayLine- value"
-        else
-            array[ 0 ].Split()
+                    if vertexOne = vertexTwo then
+                        (vertexOne, vertexTwo, converter currentLine[2]) :: (tripleListFormation <| Seq.removeAt 0 sequence)
+                    else
+                        (vertexOne, vertexTwo, converter currentLine[2]) :: (vertexTwo, vertexOne, converter currentLine[2]) :: (tripleListFormation <| Seq.removeAt 0 sequence)
 
-    let weight, symmetry =
-        if zeroArrayLine.Length <> 5 then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                 Error in -weight, symmetry- value"
-        else
-            zeroArrayLine[ 3 ].ToLower(), zeroArrayLine[ 4 ].ToLower() = "symmetric"
+        Matrix(tripleListFormation <| Seq.removeAt 0 dataLinesSeq, rows, columns)
 
-    let cutArray = commentsCutter array
-
-    let zeroCutArrayLine =
-        if Array.tryHead cutArray = Option.None then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                 Error in -zeroCutArrayLine- value"
-        else
-            cutArray[ 0 ].Split()
-
-    let rows, columns =
-        if zeroCutArrayLine.Length <> 3 then
-            failwith
-                "Incorrect file was given. Expected MatrixMarket format file.\n
-                 Error in -rows, columns- value"
-        else
-            uint zeroCutArrayLine[0], uint zeroCutArrayLine[1]
-
-    member this.Array = array
-    member this.Weight = weight
-    member this.IsMtxSymmetric = symmetry
-    member this.CutArray = cutArray
+    member this.ReadLines = allLinesSeq
+    member this.ReadDataLines = dataLinesSeq
+    member this.WeightType = weightType
+    member this.IsSymmetric = isSymmetric
     member this.Rows = rows
     member this.Columns = columns
+    member this.ToMatrix = matrix*)
 
-let symmetryIntMtxToGraph (array: array<string>) rows columns =
-
-    let rec listOfIntFormation (array: array<string>) =
-        if array.Length = 0 then
-            []
-        else
-            let currentLine = array[ 0 ].Split()
-            let vertexOne = uint currentLine[0] - 1u
-            let vertexTwo = uint currentLine[1] - 1u
-            let weight = Some(int currentLine[2])
-
-            if vertexOne = vertexTwo then
-                (vertexOne, vertexTwo, weight) :: (listOfIntFormation array[1..])
-            else
-                (vertexOne, vertexTwo, weight) :: (vertexTwo, vertexOne, weight) :: (listOfIntFormation array[1..])
-
-    Matrix(listOfIntFormation array[1..], rows, columns)
-
-let symmetryDoubleMtxToGraph (array: array<string>) rows columns =
-
-    let rec listOfDoubleFormation (array: array<string>) =
-        if array.Length = 0 then
-            []
-        else
-            let currentLine = array[ 0 ].Split()
-            let vertexOne = uint currentLine[0] - 1u
-            let vertexTwo = uint currentLine[1] - 1u
-            let weight = Some(float currentLine[2])
-
-            if vertexOne = vertexTwo then
-                (vertexOne, vertexTwo, weight) :: (listOfDoubleFormation array[1..])
-            else
-                (vertexOne, vertexTwo, weight)
-                :: (vertexTwo, vertexOne, weight) :: (listOfDoubleFormation array[1..])
-
-    Matrix(listOfDoubleFormation array[1..], rows, columns)
-
-let symmetryPatternMtxToGraph (array: array<string>) rows columns =
-
-    let rec listOfUnitFormation (array: array<string>) =
-        if array.Length = 0 then
-            []
-        else
-            let currentLine = array[ 0 ].Split()
-            let vertexOne = uint currentLine[0] - 1u
-            let vertexTwo = uint currentLine[1] - 1u
-
-            if vertexOne = vertexTwo then
-                (vertexOne, vertexTwo, Some()) :: (listOfUnitFormation array[1..])
-            else
-                (vertexOne, vertexTwo, Some()) :: (vertexTwo, vertexOne, Some()) :: (listOfUnitFormation array[1..])
-
-    Matrix(listOfUnitFormation array[1..], rows, columns)
