dcb36b2335ef0449a327980e3ce7bba55f141378
PolinaSavelyeva <lissava00@gmail.com>
2023-02-26 11:25:51 +0300

    Add verticesCount for Graph

--- src/ProgrammingHomeworkFs/Graph.fs
+++ src/ProgrammingHomeworkFs/Graph.fs
@@ -6,23 +6,30 @@ open MtxReader
 type Graph<'Value when 'Value: equality> =
 
     val AdjacencyMatrix: Matrix<'Value>
-    val squareTypeFailWith: unit
+    val VerticesCount: uint
 
-    new(adjacencyMatrix: Matrix<'Value>) =
-        { squareTypeFailWith =
+    new(adjacencyMatrix) =
+        { AdjacencyMatrix = adjacencyMatrix
+          VerticesCount =
             if adjacencyMatrix.Length1 <> adjacencyMatrix.Length2 then
                 failwith
                     $"Incorrect matrix type was given.\n
                                Expected length1 : %A{adjacencyMatrix.Length1} = length2 : %A{adjacencyMatrix.Length2}."
-          AdjacencyMatrix = adjacencyMatrix }
+            else
+                adjacencyMatrix.Length1 }
 
     new(tripleList, verticesCount) =
-        { squareTypeFailWith = ()
-          AdjacencyMatrix = Matrix(tripleList, verticesCount, verticesCount) }
+        { AdjacencyMatrix = Matrix(tripleList, verticesCount, verticesCount)
+          VerticesCount = verticesCount }
 
     new(mtxFile: MtxFile, converter) =
-        { squareTypeFailWith = () // Matrix from MatrixMarket file supposed to be square
-          AdjacencyMatrix = toSparseMatrix converter mtxFile }
+        { AdjacencyMatrix = toSparseMatrix converter mtxFile
+          VerticesCount =
+            if mtxFile.Rows <> mtxFile.Columns then
+                failwith
+                    $"Incorrect MatrixMarket file was given.\n
+                    Expected rows : %A{mtxFile.Rows} = columns : %A{mtxFile.Columns}."
+            else
+                mtxFile.Rows }
 
-    member this.VerticesCount = this.AdjacencyMatrix.Length1
     member this.GetEdge(u, v) = this.AdjacencyMatrix[u, v]
