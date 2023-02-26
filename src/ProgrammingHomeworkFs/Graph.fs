eef3423f4c8b2ccbf1aa5ef14da0714efcd883ce
PolinaSavelyeva <lissava00@gmail.com>
2023-02-26 10:30:32 +0300

    Add formation

--- src/ProgrammingHomeworkFs/Graph.fs
+++ src/ProgrammingHomeworkFs/Graph.fs
@@ -6,16 +6,23 @@ open MtxReader
 type Graph<'Value when 'Value: equality> =
 
     val AdjacencyMatrix: Matrix<'Value>
-    val VerticesCount: uint
+    val squareTypeFailWith: unit
 
-    new(adjacencyMatrix, verticesCount) =
-        { AdjacencyMatrix = adjacencyMatrix
-          VerticesCount = verticesCount }
+    new(adjacencyMatrix: Matrix<'Value>) =
+        { squareTypeFailWith =
+            if adjacencyMatrix.Length1 <> adjacencyMatrix.Length2 then
+                failwith
+                    $"Incorrect matrix type was given.\n
+                               Expected length1 : %A{adjacencyMatrix.Length1} = length2 : %A{adjacencyMatrix.Length2}."
+          AdjacencyMatrix = adjacencyMatrix }
 
     new(tripleList, verticesCount) =
-        { AdjacencyMatrix = Matrix(tripleList, verticesCount, verticesCount)
-          VerticesCount = verticesCount }
+        { squareTypeFailWith = ()
+          AdjacencyMatrix = Matrix(tripleList, verticesCount, verticesCount) }
 
     new(mtxFile: MtxFile, converter) =
-        { AdjacencyMatrix = toSparseMatrix converter mtxFile
-          VerticesCount = mtxFile.Rows }
+        { squareTypeFailWith = () // Matrix from MatrixMarket file supposed to be square
+          AdjacencyMatrix = toSparseMatrix converter mtxFile }
+
+    member this.VerticesCount = this.AdjacencyMatrix.Length1
+    member this.GetEdge(u, v) = this.AdjacencyMatrix[u, v]
